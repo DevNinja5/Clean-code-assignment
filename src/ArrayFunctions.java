6418b7e99405cafcafd09869ff125cecc319c7e5
Rahul Soni <rahul.soni@knoldus.com>
2021-08-05 15:03:57 +0530

    Initial commit and warnings fixed in ArraysFunction.java file

---
+++ src/ArrayFunctions.java
@@ -0,0 +1,43 @@
+public class ArrayFunctions {
+    static class Number {
+        int smallest;
+        int largest;
+    }
+
+    static Number findLargestSmallest(int[] numPool, int poolCount) {
+        Number num = new Number();
+        int index;
+
+        if (poolCount == 1) {
+            num.largest = numPool[0];
+            num.smallest = numPool[0];
+            return num;
+        }
+
+        if (numPool[0] > numPool[1]) {
+            num.largest = numPool[0];
+            num.smallest = numPool[1];
+        } else {
+            num.largest = numPool[1];
+            num.smallest = numPool[0];
+        }
+
+        for (index = 2; index < poolCount; index++) {
+            if (numPool[index] > num.largest) {
+                num.largest = numPool[index];
+            } else if (numPool[index] < num.smallest) {
+                num.smallest = numPool[index];
+            }
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int[] numPool = {1000, 11, 445, 1, 330, 3000};
+        int poolCount = 6;
+        Number num = findLargestSmallest(numPool, poolCount);
+        System.out.printf("\nSmallest Number is %d", num.smallest);
+        System.out.printf("\nLargest Number is %d", num.largest);
+    }
+}
